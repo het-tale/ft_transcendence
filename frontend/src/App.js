2c5d0fd228675fe5f79acc012d22e3d48be9a779
Hasnaa Et-taleby <het-tale@c3r6p10.1337.ma>
2023-08-14 18:51:40 +0100

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="flex flex-col">
+      <navbar>
+        <div>
+          <span className="font-krona text-xs">TwinP</span><img src="/assets/twinpong.png" className="w-6" alt="pingpong"/>
+          <span>ng</span>
+        </div>
+      </navbar>
+      <main>
+        world
+      </main>
+    </div>
+  );
+}
+
+export default App;
