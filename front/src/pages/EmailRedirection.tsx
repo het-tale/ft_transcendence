8b8e596005149e01d2ad5e71454f5e1e34bd69e3
het-tale <ettalebyhasnaa99@gmail.com>
2023-09-12 08:50:56 +0100

    email link good

--- front/src/pages/EmailRedirection.tsx
+++ front/src/pages/EmailRedirection.tsx
@@ -3,19 +3,29 @@ import '../css/login.css';
 import client from "../components/Client";
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from 'react';
+import { useToast } from '@chakra-ui/react';
 
 
 function EmailRedirection() {
     let navigate = useNavigate();
     const location = useLocation();
+    const toast = useToast();
     const Email = async () => {
     const token = location.search.split('=')[1];
         try {
 
         const response = await client.get("/auth/confirm-email?token=" + token);                 
         if (response.status === 200) {
-            navigate('/login');
-            // navigate('/complete-profile');
+            toast({
+                title: 'Email Confirmed.',
+                description: "Your email address has been successfully verified.",
+                status: 'success',
+                duration: 9000,
+                isClosable: true,
+                position: "bottom-right",
+              })
+            // navigate('/login');
+            navigate('/complete-profile');
         }
         }
         catch (error : any) {
@@ -24,10 +34,26 @@ function EmailRedirection() {
             if (errorMessage === 'email already confirmed' && errorStatus === 403)
             {
                 // navigate('/home');
+                toast({
+                    title: 'Email already Confirmed.',
+                    description: "Your email address has been already verified.",
+                    status: 'success',
+                    duration: 9000,
+                    isClosable: true,
+                    position: "bottom-right",
+                  })
                 navigate('/login');
             }
             else
             {
+                toast({
+                    title: 'Resend Email.',
+                    description: "An error occured during verification get an other email.",
+                    status: 'success',
+                    duration: 9000,
+                    isClosable: true,
+                    position: "bottom-right",
+                  })
                 navigate('/confirm-email');
             }
         }
