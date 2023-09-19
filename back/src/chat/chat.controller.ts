578697077dcd34a113131f7d9e4155862d391405
Sara Lahrach <slahrach@student.1337.ma>
2023-09-19 04:21:43 +0100

    feat: add friend remove friend block unblock

--- back/src/chat/chat.controller.ts
+++ back/src/chat/chat.controller.ts
@@ -5,6 +5,7 @@ import { EmailConfirmationGuard } from 'src/guards/email-confirmation.guard';
 import { TwoFaVerificationGuard } from 'src/guards/two-fa-verification.guard';
 import { ChannelService } from './channel.service';
 import { User } from '@prisma/client';
+import { FriendsService } from './friends.service';
 
 @Controller('chat')
 @UseGuards(EmailConfirmationGuard)
@@ -14,6 +15,7 @@ export class ChatController {
   constructor(
     private dmService: DMService,
     private channelService: ChannelService,
+    private friendsService: FriendsService,
   ) {}
 
   @Get('dms/:username')
@@ -30,5 +32,25 @@ export class ChatController {
     @Req() request: { user: User },
   ) {
     return this.channelService.getChannelMessages(channelName, request.user);
+  }
+  @Get('friends')
+  async getFriends(@Req() request: { user: User }) {
+    return this.friendsService.getFriends(request.user);
+  }
+  @Get('blocked')
+  async getBlockedUsers(@Req() request: { user: User }) {
+    return this.friendsService.getBlockedUsers(request.user);
+  }
+  @Get('dms-list')
+  async getDmsList(@Req() request: { user: User }) {
+    return this.dmService.getDmsList(request.user);
+  }
+  @Get('my-channels-list')
+  async getMyChannelsList(@Req() request: { user: User }) {
+    return this.channelService.getMyChannelsList(request.user);
+  }
+  @Get('browse-channels-list')
+  async getBrowseChannelsList(@Req() request: { user: User }) {
+    return this.channelService.getBrowseChannelsList(request.user);
   }
 }
