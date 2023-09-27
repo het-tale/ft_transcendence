2e52b4f09a341857c581dd6d0029ca0bcb1cc2b3
het-tale <ettalebyhasnaa99@gmail.com>
2023-09-27 02:28:57 +0100

    Fix protect password

--- front/src/components/ProtectPassword.tsx
+++ front/src/components/ProtectPassword.tsx
@@ -3,55 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import User from './User';
 import Protection from './Protection';
 
-// const ProtectPassword = (props: any) => {
-//   const navigate = useNavigate();
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-//   const [isPasswordRequired, setIsPasswordRequired] = useState(false);
-
-//   useEffect(() => {
-//     const checkAuthentication = async () => {
-//       try {
-//         const response = await User();
-//         if (response !== null) {
-//           setIsLoggedIn(true);
-//           setIsPasswordRequired(response.isPasswordRequired);
-//           console.log("Password", response.isPasswordRequired);
-//         } else {
-//           navigate('/');
-//         }
-//       } catch (error) {
-//         navigate('/');
-//       }
-//     };
-
-//     checkAuthentication();
-//   },[navigate]);
-
-//   if (isLoggedIn)
-//   {
-//     if (isPasswordRequired)
-//     {
-//       return <React.Fragment>{props.children}</React.Fragment>;
-//     }
-//     else
-//     {
-//       navigate('/home');
-//     }
-//     return null;
-//   }
-//   else
-//   {
-//     return null;
-//   }
-// };
-
-// export default ProtectPassword;
-
 
 const ProtectPassword = (props: any) =>
 {
   return (
-  <Protection name="password" />);
+  <Protection name="password" child={props.children}/>);
 }
       
 export default ProtectPassword;
