d8d32efd32cf2f92caeee91e2033a0863a80b995
het-tale <ettalebyhasnaa99@gmail.com>
2023-10-03 00:19:46 +0100

    Fix: solve the socket issue

--- front/src/pages/Signin42.tsx
+++ front/src/pages/Signin42.tsx
@@ -1,6 +1,9 @@
-import { error } from "console";
-import User from "../components/User";
-import { useLocation, useNavigate } from "react-router-dom";
+import { error } from 'console';
+import User from '../components/User';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { SocketContext } from '../socket';
+import React from 'react';
 
 const Signin42 = () => {
     const navigate = useNavigate();
@@ -9,28 +12,25 @@ const Signin42 = () => {
     const token = location.search.split('=')[1];
     console.log(token);
     localStorage.setItem('token', token);
+    const socket = React.useContext(SocketContext);
+    useEffect(() => {
+        socket.auth = { token: token };
+    }, []);
     User()
-    .then((res) => {
-        console.log("User", res);
-        console.log("Password", res.isPasswordRequired);
-        if (res.isPasswordRequired === true)
-        {
-            navigate('/set-password');
-        }
-        else
-        {
-            navigate('/home');
-        }
-    })
-    .catch((error) => {
-        console.log("Error", error);
-        navigate('/');
-    })
-    return (
-        <div>
-            
-        </div>
-    );
+        .then((res) => {
+            console.log('User', res);
+            console.log('Password', res.isPasswordRequired);
+            if (res.isPasswordRequired === true) {
+                navigate('/set-password');
+            } else {
+                navigate('/home');
+            }
+        })
+        .catch((error) => {
+            console.log('Error', error);
+            navigate('/');
+        });
+    return <div></div>;
 };
 
 export default Signin42;
