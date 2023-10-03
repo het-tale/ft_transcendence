9b335aacaa9858eced6da7e811e6bcc3d85baf08
het-tale <ettalebyhasnaa99@gmail.com>
2023-10-04 00:38:18 +0100

    Fix: solve socket issue

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,13 +1,20 @@
 import '../css/landing.css';
 import '../css/sidebar.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Sidebar from '../components/Sidebar';
 import NavbarSearch from '../components/NavbarSearch';
 import { Flex } from '@chakra-ui/react';
+import { SocketContext } from '../socket';
 
 function Home() {
+    const socket = React.useContext(SocketContext);
+    const token = localStorage.getItem('token');
+    useEffect(() => {
+        socket.auth = { token: token };
+        socket.connect();
+    }, []);
     return (
-        <Flex flexDirection={"column"}>
+        <Flex flexDirection={'column'}>
             <NavbarSearch />
             <Sidebar />
         </Flex>
--- front/src/pages/Signin42.tsx
+++ front/src/pages/Signin42.tsx
@@ -2,7 +2,7 @@ import { error } from 'console';
 import User from '../components/User';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
-import { SocketContext } from '../socket';
+
 import React from 'react';
 
 const Signin42 = () => {
@@ -12,10 +12,6 @@ const Signin42 = () => {
     const token = location.search.split('=')[1];
     console.log(token);
     localStorage.setItem('token', token);
-    const socket = React.useContext(SocketContext);
-    useEffect(() => {
-        socket.auth = { token: token };
-    }, []);
     User()
         .then((res) => {
             console.log('User', res);
