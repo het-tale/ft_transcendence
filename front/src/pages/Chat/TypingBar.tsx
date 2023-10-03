49a997839c1d629a043aecfc76dd2259f8616faf
het-tale <ettalebyhasnaa99@gmail.com>
2023-10-03 01:55:24 +0100

    The chat is working/ still need to handle user status && and fix toast issue

--- front/src/pages/Chat/TypingBar.tsx
+++ front/src/pages/Chat/TypingBar.tsx
@@ -5,6 +5,7 @@ import { set } from 'react-hook-form';
 import { useToast } from '@chakra-ui/react';
 import { render } from '@testing-library/react';
 import { io } from 'socket.io-client';
+import SocketListen from './socketListen';
 
 const TypingBar = (props: any) => {
     const [message, setMessage] = useState('');
@@ -20,14 +21,37 @@ const TypingBar = (props: any) => {
         setMessage('');
         props.setRender(!props.render);
     };
-    useEffect(() => {
-        socket.on('privateMessage', (data: any) => {
-            console.log('MESSAGE DATA', data);
-            // props.setRender(!props.render);
-            // setMessage(data.message);
-            // from: data.from;
+    // useEffect(() => {
+    socket.on('privateMessage', (data: any) => {
+        console.log('MESSAGE DATA', data);
+        props.setRender(!props.render);
+    });
+    socket.on('privateMessageError', (data: any) => {
+        console.log('MESSAGE ERROR DATA', data);
+        toast({
+            title: 'Error',
+            description: data,
+            status: 'error',
+            duration: 9000,
+            isClosable: true,
+            position: 'top-right'
         });
-    }, [message]);
+    });
+    socket.on('userOffline', (data: any) => {
+        console.log('USER OFFLINE', data);
+        props.setRender(!props.render);
+    });
+    socket.on('userOnline', (data: any) => {
+        console.log('USER ONLINE', data);
+        props.setRender(!props.render);
+    });
+    // socket.on('privateMessage', (data: any) => {
+    //     console.log('MESSAGE DATA', data);
+    //     props.setRender(!props.render);
+    //     // setMessage(data.message);
+    //     // from: data.from;
+    // });
+    // }, []);
     return (
         <form className="typing-bar" onSubmit={sendMessageHandler}>
             <input
