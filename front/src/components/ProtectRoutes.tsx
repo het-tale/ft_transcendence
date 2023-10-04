14e31d4df9cb3617525dc3b8ebf7570f12401853
het-tale <ettalebyhasnaa99@gmail.com>
2023-10-04 23:17:46 +0100

    Fix: The socket issue and handle multi-rendring

--- front/src/components/ProtectRoutes.tsx
+++ front/src/components/ProtectRoutes.tsx
@@ -1,21 +1,25 @@
 import client from './Client';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { SocketContext } from '../socket';
 
 const ProtectRoutes = (props: any) => {
     const navigate = useNavigate();
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+    const socket = React.useContext(SocketContext);
+    const token = localStorage.getItem('token');
     useEffect(() => {
         const checkAuthentication = async () => {
             try {
                 const response = await client.get('/user/me', {
                     headers: {
-                        Authorization: 'Bearer ' + localStorage.getItem('token')
+                        Authorization: 'Bearer ' + token
                     }
                 });
                 if (response.status === 200) {
                     setIsLoggedIn(true);
+                    socket.auth = { token: token };
+                    socket.connect();
                 } else {
                     navigate('/');
                 }
