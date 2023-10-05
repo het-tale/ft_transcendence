9a18f4e2f0b7a0700b057eca49189338c5c45ca9
Sara Lahrach <slahrach@student.1337.ma>
2023-10-05 01:29:40 +0100

    remove front to merge the backend only

--- front/src/components/ProtectConfirmation.tsx
+++
@@ -1,9 +0,0 @@
-import Protection from "./Protection";
-
-const ProtectConfirmation = (props: any) => {
-    return (
-        <Protection name="confirmation" child={props.children}/>
-    );
-}
-
-export default ProtectConfirmation;
