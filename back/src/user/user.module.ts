c0f1c675fc178df4b3f7387a146b6c03a425ee5c
Sara Lahrach <slahrach@student.1337.ma>
2023-10-12 23:48:01 +0100

    fix: remove security service from user controller

--- back/src/user/user.module.ts
+++ back/src/user/user.module.ts
@@ -3,10 +3,9 @@ import { TwoFaModule } from 'src/2fa/two-fa.module';
 import { ConfirmationModule } from 'src/confirmation/confirmation.module';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TwoFaModule, ConfirmationModule, AuthModule],
+  imports: [TwoFaModule, ConfirmationModule],
   controllers: [UserController],
   providers: [UserService],
 })
