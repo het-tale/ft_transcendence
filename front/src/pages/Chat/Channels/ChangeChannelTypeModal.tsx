b78b067aebf7c4e32f7e81ae274b286953359d69
het-tale <ettalebyhasnaa99@gmail.com>
2023-10-14 00:46:13 +0100

    Feat: Change channel type && add unban users list

---
+++ front/src/pages/Chat/Channels/ChangeChannelTypeModal.tsx
@@ -0,0 +1,154 @@
+import React from 'react';
+import '../../../css/chat/modal.css';
+import { Button, ButtonGroup, Input } from '@chakra-ui/react';
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Radio,
+    RadioGroup,
+    HStack,
+    useToast
+} from '@chakra-ui/react';
+
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { SocketContext } from '../../../socket';
+import client from '../../../components/Client';
+
+interface ChangeTypeData {
+    type: string;
+    password?: string;
+}
+
+interface ChangeChannelTypeModalProps {
+    onClose: () => void;
+    setRender?: React.Dispatch<React.SetStateAction<boolean>>;
+    render?: boolean;
+    channelDm?: any;
+    user?: any;
+}
+
+const ChangeChannelTypeModal = (props: ChangeChannelTypeModalProps) => {
+    const socket = React.useContext(SocketContext);
+    const [selectedOption, setSelectedOption] = React.useState('');
+    const [showField, setShowField] = React.useState(false);
+    const toast = useToast();
+    const handleRadioChange = (event: any) => {
+        setSelectedOption(event.target.value);
+        setShowField(event.target.value === 'protected');
+        console.log('RADIOOOOOOO CHAAAANGE', event.target.value);
+    };
+    const { register, handleSubmit } = useForm<ChangeTypeData>();
+    const handleChangeChannelType: SubmitHandler<ChangeTypeData> = async (
+        data
+    ) => {
+        console.log('Change Channel Type Data', data);
+        if (!showField) data.password = undefined;
+
+        const sentData = {
+            name: props.channelDm?.name,
+            password: data.password,
+            type: data.type
+        };
+        try {
+            const response = await client.post(
+                `chat/change-channel-type/`,
+                sentData,
+                {
+                    headers: {
+                        Authorization: 'Bearer ' + localStorage.getItem('token')
+                    }
+                }
+            );
+            props.setRender && props.setRender(!props.render);
+            props.onClose();
+        } catch (error: any) {
+            console.log('error', error);
+            toast({
+                title: 'Error',
+                description: error.response.data.message,
+                status: 'error',
+                duration: 9000,
+                isClosable: true,
+                position: 'bottom-right'
+            });
+            props.onClose();
+        }
+    };
+    return (
+        <form
+            style={{ padding: '5px' }}
+            onSubmit={handleSubmit(handleChangeChannelType)}
+        >
+            <FormControl as="fieldset">
+                <FormLabel>Channel Type</FormLabel>
+                <RadioGroup>
+                    <HStack spacing="24px">
+                        <Radio
+                            value="public"
+                            checked={selectedOption === 'public'}
+                            {...register('type', {
+                                onChange: handleRadioChange
+                            })}
+                        >
+                            Public
+                        </Radio>
+                        <Radio
+                            value="private"
+                            checked={selectedOption === 'private'}
+                            {...register('type', {
+                                onChange: handleRadioChange
+                            })}
+                        >
+                            Private
+                        </Radio>
+                        <Radio
+                            value="protected"
+                            checked={selectedOption === 'protected'}
+                            {...register('type', {
+                                onChange: handleRadioChange
+                            })}
+                        >
+                            Protected
+                        </Radio>
+                    </HStack>
+                </RadioGroup>
+            </FormControl>
+            {showField ? (
+                <div className="form-group">
+                    <label htmlFor="password">Channel Password</label>
+                    <input
+                        className="form-control"
+                        type="password"
+                        placeholder="Type password"
+                        id="password"
+                        required
+                        {...register('password')}
+                    />
+                </div>
+            ) : null}
+            <ButtonGroup display={'Flex'} justifyContent={'flex-end'} p={3}>
+                <Button
+                    bg={'#E9ECEF'}
+                    color={'white'}
+                    mr={3}
+                    onClick={props.onClose}
+                >
+                    Close
+                </Button>
+                <Button
+                    variant="ghost"
+                    bg={'#a435f0'}
+                    color={'white'}
+                    type="submit"
+                    className="excludeSubmit"
+                >
+                    Create
+                </Button>
+            </ButtonGroup>
+        </form>
+    );
+};
+
+export default ChangeChannelTypeModal;
