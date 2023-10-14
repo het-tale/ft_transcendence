fe0bc3c43f2a10acd05b5f98955a42653c8b599e
het-tale <ettalebyhasnaa99@gmail.com>
2023-10-14 20:38:23 +0100

    display list of friends in profile

---
+++ front/src/pages/Profile/GetMutualFriendsList.tsx
@@ -0,0 +1,21 @@
+import client from '../../components/Client';
+
+export const GetMutualFriendsList = async (username: string | undefined) => {
+    try {
+        const res = await client.get(`chat/mutual-friends/${username}`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+        console.log('RES', res);
+        if (res.status === 200) {
+            console.log('Mutual Friends data', res);
+            return res.data;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.log('Error', error);
+        return null;
+    }
+};
