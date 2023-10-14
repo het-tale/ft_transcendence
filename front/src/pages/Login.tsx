5a4794c826edab95ae327fba26aceed07d1d4455
Mohammed Chliyah <mchliyah@e2r13p11.1337.ma>
2023-10-14 02:38:41 +0100

    trying to set urls => .env

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -40,7 +40,7 @@ function Login(props: any) {
         setErrorMessage('');
         const configuration = {
             method: 'post',
-            url: `${process.env.BACKEND_URL}/auth/signin`,
+            url: 'http://localhost:3001/auth/signin',
             data: {
                 identifier: email,
                 password
--- front/src/socket.tsx
+++ front/src/socket.tsx
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 
 let token = localStorage.getItem('token');
 console.log('Socket token', token);
-const URL = `${process.env.BACKEND_URL}/chat`;
+const URL = 'http://localhost:3001/chat';
 
 export const socket = io(URL, {
     withCredentials: true,
