4ca8cd8ec1c9f29f1c041712ffcde914e7511c01
het-tale <ettalebyhasnaa99@gmail.com>
2023-10-20 17:05:27 +0100

    Fix: change username using sockets

--- front/src/pages/Profile/EditUserNameBody.tsx
+++ front/src/pages/Profile/EditUserNameBody.tsx
@@ -5,6 +5,8 @@ import '../../css/chat/modal.css';
 import { Button, ButtonGroup, useToast } from '@chakra-ui/react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { RenderContext } from '../../RenderContext';
+import React from 'react';
+import { SocketContext } from '../../socket';
 
 interface EditUserNameBodyProps {
     onClose: () => void;
@@ -18,37 +20,42 @@ const EditUserNameBody = (props: EditUserNameBodyProps) => {
     const toast = useToast();
     const renderData = useContext(RenderContext);
     const { register, handleSubmit } = useForm<EditUserNameData>();
+    const socket = React.useContext(SocketContext);
     const handleEditUserName: SubmitHandler<EditUserNameData> = async (
         data
     ) => {
-        // console.log('Edit Profile data', data);
-        const sentData = {
+        socket.emit('changeUsername', {
             name: data.username
-        };
-        try {
-            const response = await client.post(
-                `user/change-username`,
-                sentData,
-                {
-                    headers: {
-                        Authorization: 'Bearer ' + localStorage.getItem('token')
-                    }
-                }
-            );
-            renderData.setRenderData(!renderData.renderData);
-            props.onClose();
-        } catch (error: any) {
-            console.log('error', error);
-            toast({
-                title: 'Error',
-                description: error.response.data.message,
-                status: 'error',
-                duration: 9000,
-                isClosable: true,
-                position: 'bottom-right'
-            });
-            props.onClose();
-        }
+        });
+        renderData.setRenderData(!renderData.renderData);
+        // console.log('Edit Profile data', data);
+        // const sentData = {
+        //     name: data.username
+        // };
+        // try {
+        //     const response = await client.post(
+        //         `user/change-username`,
+        //         sentData,
+        //         {
+        //             headers: {
+        //                 Authorization: 'Bearer ' + localStorage.getItem('token')
+        //             }
+        //         }
+        //     );
+        //     renderData.setRenderData(!renderData.renderData);
+        //     props.onClose();
+        // } catch (error: any) {
+        //     console.log('error', error);
+        //     toast({
+        //         title: 'Error',
+        //         description: error.response.data.message,
+        //         status: 'error',
+        //         duration: 9000,
+        //         isClosable: true,
+        //         position: 'bottom-right'
+        //     });
+        //     props.onClose();
+        // }
     };
     return (
         <form
