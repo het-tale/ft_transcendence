0bf0eed8dde3631e6a89fba612a417931d32fd4d
het-tale <ettalebyhasnaa99@gmail.com>
2023-11-01 03:37:40 +0100

    Fix: get specific error from backend when changing channel type

--- front/src/pages/Chat/Channels/ChangeChannelTypeModal.tsx
+++ front/src/pages/Chat/Channels/ChangeChannelTypeModal.tsx
@@ -54,14 +54,28 @@ const ChangeChannelTypeModal = (props: ChangeChannelTypeModalProps) => {
             props.setRender && props.setRender(!props.render);
             props.onClose();
         } catch (error: any) {
-            toast({
-                title: 'Error',
-                description: error.response.data.message,
-                status: 'error',
-                duration: 9000,
-                isClosable: true,
-                position: 'bottom-right'
-            });
+            const errors = error?.response?.data?.errors;
+            if (errors) {
+                for (let index = 0; index < errors?.length; index++) {
+                    toast({
+                        title: 'Error.',
+                        description: errors[index].message,
+                        status: 'error',
+                        duration: 5000,
+                        isClosable: true,
+                        position: 'bottom-right'
+                    });
+                }
+            } else {
+                toast({
+                    title: 'Error.',
+                    description: error?.response?.data?.message,
+                    status: 'error',
+                    duration: 5000,
+                    isClosable: true,
+                    position: 'bottom-right'
+                });
+            }
             props.onClose();
         }
     };
